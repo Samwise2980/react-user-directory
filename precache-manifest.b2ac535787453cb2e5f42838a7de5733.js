c5acc1ffbb77baa2cbadac85d9f216aa04ca15cc
Samwise <samuelfoster0@gmail.com>
2020-05-08 21:34:45 -0700

    Updates

---
+++ precache-manifest.b2ac535787453cb2e5f42838a7de5733.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1fb1c84334b626732d1fbc8cc0f2cb",
+    "url": "/react-user-directory/index.html"
+  },
+  {
+    "revision": "45bc5f86c25bf3bf05cc",
+    "url": "/react-user-directory/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "988bfe995c49a66118e5",
+    "url": "/react-user-directory/static/css/main.2dbfbee2.chunk.css"
+  },
+  {
+    "revision": "45bc5f86c25bf3bf05cc",
+    "url": "/react-user-directory/static/js/2.f971c6c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-user-directory/static/js/2.f971c6c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "988bfe995c49a66118e5",
+    "url": "/react-user-directory/static/js/main.2938689b.chunk.js"
+  },
+  {
+    "revision": "592bed523b213c5b53b6",
+    "url": "/react-user-directory/static/js/runtime-main.4d833f6e.js"
+  }
+]);
